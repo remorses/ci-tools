d9396430698d68aee1d05bfdec243a8401a00559
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-21 21:45:00 +0100

    i am still learning

--- .circleci/suitable.sh
+++ .circleci/suitable.sh
@@ -2,4 +2,4 @@ set -e
 
 LAST_SUITABLE=`git log -n 1 -i --no-merges --invert-grep --grep="skip ci" --pretty=format:%h -- $(find $1 | tr '\n' ' ')`
 LAST=`git log --pretty=format:'%h' -n 1`
-[ $LAST = $LAST_SUITABLE ] || exit 1 && echo "echo ok"
+[ $LAST = $LAST_SUITABLE ] || exit 1 && echo "ok"
