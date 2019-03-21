8684541bf003120575373c9d2306c982997261eb
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-21 18:46:52 +0100

    added -x to debug

--- .circleci/suitable.sh
+++ .circleci/suitable.sh
@@ -1,5 +1,7 @@
-set -e
+set -ex
 
 LAST_SUITABLE=`git log -n 1 --pretty=format:%h -- $1` 
 LAST=`git log --pretty=format:'%h' -n 1`
+echo "LAST = $LAST"
+echo "LAST_SUITABLE = $LAST_SUITABLE"
 [ $LAST = $LAST_SUITABLE ] || exit 1 && exit 0
