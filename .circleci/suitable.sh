e2fbd2bde0128327a6aebc0d3be2c8871662b119
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-21 20:00:37 +0100

    update

--- .circleci/suitable.sh
+++ .circleci/suitable.sh
@@ -1,5 +1,5 @@
 set -ex
 
-LAST_SUITABLE=`git log -n 1 --pretty=format:%h -- $(find $1 -print0)`
+LAST_SUITABLE=`git log -n 1 --pretty=format:%h -- $(find $1 | tr '\n' ' ')`
 LAST=`git log --pretty=format:'%h' -n 1`
 [ $LAST = $LAST_SUITABLE ] || exit 1 && exit 0
