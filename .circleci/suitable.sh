59bff8f981a3b27ff1d694553474e695cb556798
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-21 21:16:41 +0100

    removed ci commits from suitable set

--- .circleci/suitable.sh
+++ .circleci/suitable.sh
@@ -1,5 +1,5 @@
 set -ex
 
-LAST_SUITABLE=`git log -n 1 --pretty=format:%h -- $(find $1 | tr '\n' ' ')`
+LAST_SUITABLE=`git log -n 1 -i --no-merges --invert-grep --grep="skip ci" --pretty=format:%h -- $(find $1 | tr '\n' ' ')`
 LAST=`git log --pretty=format:'%h' -n 1`
 [ $LAST = $LAST_SUITABLE ] || exit 1
