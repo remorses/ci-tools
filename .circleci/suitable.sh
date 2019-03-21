79ea5cd4ef050f5508b6d2fdc0e48e935cdcb055
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-21 21:20:31 +0100

    small fix

--- .circleci/suitable.sh
+++ .circleci/suitable.sh
@@ -2,4 +2,4 @@ set -ex
 
 LAST_SUITABLE=`git log -n 1 -i --no-merges --invert-grep --grep="skip ci" --pretty=format:%h -- $(find $1 | tr '\n' ' ')`
 LAST=`git log --pretty=format:'%h' -n 1`
-[ $LAST = $LAST_SUITABLE ] || exit 1
+[ $LAST = $LAST_SUITABLE ] || exit 1 && echo "echo ok"
