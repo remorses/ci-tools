84c8d0999545dde71717d37b1b202d28661d3883
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-21 20:59:58 +0100

    should work now

--- .circleci/suitable.sh
+++ .circleci/suitable.sh
@@ -2,4 +2,4 @@ set -ex
 
 LAST_SUITABLE=`git log -n 1 --pretty=format:%h -- $(find $1 | tr '\n' ' ')`
 LAST=`git log --pretty=format:'%h' -n 1`
-[ $LAST = $LAST_SUITABLE ] || exit 1 && echo suitable
+[ $LAST = $LAST_SUITABLE ] || exit 1
